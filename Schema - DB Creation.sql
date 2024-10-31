51c8c1300cc2bc2db59472d47e209e1422dbace3
Agha Salik Ali <salikali2013@gmail.com>
2024-10-31 15:34:18 +0500

    added admins tables

--- Schema - DB Creation.sql
+++ Schema - DB Creation.sql
@@ -4,6 +4,13 @@ CREATE TABLE Departments (
     CONSTRAINT pk_departments PRIMARY KEY (Department_ID)
 );
 
+CREATE TABLE Admins (
+    admin_id INT NOT NULL AUTO_INCREMENT,
+    password_hash VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_admins PRIMARY KEY (admin_id)
+) AUTO_INCREMENT = 3000;
+
+
 CREATE TABLE Users (
     User_ID INT NOT NULL AUTO_INCREMENT,
     Email VARCHAR(255) NOT NULL,
